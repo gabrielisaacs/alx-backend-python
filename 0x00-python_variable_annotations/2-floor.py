753d2250fc52fd4296900b2708dbaca1ea1dbeec
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-11 09:32:35 +0100

    floor type-annot*n

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function to floor a float
+"""
+import math
+
+
+def floor(n: float) -> float:
+    """
+    floor the float n
+    args:
+        n: float
+    """
+    return math.floor(n)
