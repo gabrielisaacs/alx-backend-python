d7326c157f1ce6aec681c612444c1d50a5c905bf
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-11 20:40:28 +0100

    fix multiplier function

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,10 +4,8 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Returns a function that multiplies a float by the given multiplier."""
-    
     def multiplier_function(value: float) -> float:
         """Multiplies the input value by the multiplier."""
         return value * multiplier
 
     return multiplier_function
-
