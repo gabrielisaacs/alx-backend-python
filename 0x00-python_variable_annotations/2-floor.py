e6ebe38fc5cf98fa8ea0d9275b5ecdfcb15a83e5
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-11 09:43:36 +0100

    fix floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -5,10 +5,13 @@ Type-annotated function to floor a float
 import math
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """
     floor the float n
+
     args:
         n: float
+    returns:
+        int
     """
     return math.floor(n)
