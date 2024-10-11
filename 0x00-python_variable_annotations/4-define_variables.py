dce7ab2d41793e6046cc199dceade43d463e31ac
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-11 10:48:32 +0100

    fix variable annotations

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,44 +1,10 @@
 #!/usr/bin/env python3
 """
-define and type-annotate variables
+Define and annotate variables
 """
 
 
-def a(1: int) -> int:
-    """
-    args:
-        1: int
-    returns:
-        integer
-    """
-    return a
-
-
-def pi(3.14: float) -> float:
-    """
-    args:
-        3.14: float
-    returns:
-        float number
-    """
-    return pi
-
-
-def i_understand_annotations(True: bool) -> bool:
-    """
-    args:
-        True: boolean
-    returns:
-        boolean
-    """
-    return i_understand_annotations
-
-
-def school("Holberton": str) -> str:
-    """
-    args:
-        Holberton: string
-    returns:
-        string
-    """
-    return school
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
