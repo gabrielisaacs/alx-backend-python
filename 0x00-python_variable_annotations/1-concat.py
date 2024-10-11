49d20ed12eba4f34d5e3f94ca317210cb4f2cda3
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-11 08:25:35 +0100

    concat string type-annot*n

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function to concat two string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concat: two string variables
+    args:
+        str1: str
+        str2: str
+    """
+    return str1 + str2
