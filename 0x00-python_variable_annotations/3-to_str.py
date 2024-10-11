c818150877063093de56ef216a2ef806a8e11e45
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-11 09:55:02 +0100

    float to str type-annot*n

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function that takes a float
+and returns a string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Turns float to str
+    args:
+        n: float
+    returns:
+        str
+    """
+    return str(n)
