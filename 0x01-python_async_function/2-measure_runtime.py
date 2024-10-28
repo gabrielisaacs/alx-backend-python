994cc4d8b8a9054087132f08db4ee3260674f6d6
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-28 13:06:13 +0100

    measure wait time

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+function to measure execution time with integers
+"""
+
+import time
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measures the total execution time for running wait_n(n, max_delay)
+    and returns the average time per call
+    args:
+        n (int): The number of calls to wait_n
+        max_delay (int): The maximum delay for each call
+
+    returns:
+        float: The average time for each call
+    """
+    start_time: float = time.time()
+    await wait_n(n, max_delay)
+    end_time: float = time.time()
+    total_time: float = end_time - start_time
+    return total_time / n
